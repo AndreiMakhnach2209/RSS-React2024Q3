a2a50dc7aa801c447fbca6e5150e70e8a1da9950
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-07-10 02:11:34 +0300

    fix: fix image loading

--- react-project/src/components/resultCard/resultCard.tsx
+++ react-project/src/components/resultCard/resultCard.tsx
@@ -1,18 +1,39 @@
 import { Component } from "react";
 import { Pokemon } from "../../types/types";
 import "./resultCard.scss";
+import logo from "../../assets/3.svg";
 
 interface ResultsCardProps {
   data: Pokemon;
 }
 
-class ResultsCard extends Component<ResultsCardProps> {
+interface ResultsCardState {
+  imageLoaded: boolean;
+}
+
+class ResultsCard extends Component<ResultsCardProps, ResultsCardState> {
+  state: Readonly<ResultsCardState> = { imageLoaded: false };
+
+  handleImageLoad = () => {
+    this.setState({ imageLoaded: true });
+  };
+
+  componentDidUpdate(prevProps: ResultsCardProps) {
+    if (prevProps.data.id !== this.props.data.id)
+      this.setState({ imageLoaded: false });
+  }
+
   render() {
     const { data } = this.props;
     const { id, types, name } = data;
+    const { imageLoaded } = this.state;
+
     return (
-      <div className="results__card">
+      <div className="results__card" key={data.id}>
         <h2>{data.name}</h2>
+        {imageLoaded || (
+          <img className="results__card-image" alt={name} src={logo} />
+        )}
         <img
           className="results__card-image"
           alt={name}
@@ -20,6 +41,7 @@ class ResultsCard extends Component<ResultsCardProps> {
             data.sprites.other.dream_world?.front_default ??
             data.sprites.front_default
           }
+          onLoad={this.handleImageLoad}
         />
         <div className="results__card-desc">
           <span>ID: {id}</span>
